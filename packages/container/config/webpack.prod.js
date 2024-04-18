1a4231c7721b313e030d01a0948eec7c701b68ba
ShivaShankarMamilla1 <shiva.mamilla@aifalabs.com>
2024-04-18 19:40:07 +0530

    added marketing deployement

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -7,15 +7,16 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode:"production",
     output:{
-        filename:'[name].[contenthash].js'//pattern of the filename 
+        filename:'[name].[contenthash].js',//pattern of the filename
+        publicPath:"/container/latest/" //This is to prepend the file name with the path like this /container/latest/main.354357387.js
     },
     plugins:[
         new ModuleFederationPlugin({
             name:"container",//not mandatory
             remotes:{
-                marketing:`marketing@${domain}/marketing/remoteEntry.js`,//here we are assuming that our marketing project is nested inside the folder marketing in aws
+                marketing:`marketing@${domain}/marketing/latest/remoteEntry.js`,//here we are assuming that our marketing project is nested inside the folder marketing in aws and here /latest is added because that is how we added in s3
             },
-            shared:packageJson.dependencies
+            shared: packageJson.dependencies
         })
     ]
 
